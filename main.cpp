85c662b79270a42115802d211d449d247f16a579
DrrJazzhands <loganjac000@hsestudents.org>
2022-11-27 19:52:09 -0500

    11/27/22
    Created the rooms and assigned the x and y vaules

--- main.cpp
+++ main.cpp
@@ -2,17 +2,32 @@
 using namespace std;
 class Room { 
     public:
-        string type;
+        inline static int NumOfRooms;
+        int XCord;
+        int YCord;
         Room(){
-            cout << "Created" << endl;
+            NumOfRooms ++;
+            // When creating the 55 rooms, keeps track of how many have been created.
+            YCord = (NumOfRooms - 1) / 11; 
+            // When creating the 55 rooms this assigns y values
+            if (NumOfRooms % 11 - 1 != -1){
+                XCord = NumOfRooms % 11 - 1;
+            }
+            else {
+                XCord = 10;
+            }
+            // When creating the 55 rooms this assigns x values
         }
-};
-class Maze {
-    public:
-        Room rooms[5][11];
 };
 int main() 
 {
-    Maze myMaze;
+    Room Rooms[5][11]; 
+    // Creates 55 Room objects. Array of 11 nested in a Array of 5. Rooms[y][x]
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 11; j++) {
+            cout << "(" << Rooms[i][j].XCord << ", " << Rooms[i][j].YCord << ")\n";
+        }
+    }
+    // Loops through all the rooms in the maze and prints out info to test. 
     return 0;
 }
